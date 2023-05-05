e0d4b1413ae2d1bd2f5e0a0562d7b7bfa6ec31b9
willklinenberg <wpk9@cornell.edu>
2023-05-05 00:22:22 -0400

    Spaces Maker Day

--- frontend/src/Spaces.js
+++ frontend/src/Spaces.js
@@ -1,7 +1,29 @@
 import React, { useState, useEffect } from "react"
 import "./App.css"; //central css file
 
-const Spaces = () => {};
+import table from './assets/Image 5-4-23 at 3.45 PM.jpeg';
+
+const Spaces = () => {
+    return(
+        <div className = "section">
+        <h1 className = "title" style = {{marginBottom: "40px",padding: "15px" }}>
+            Spaces
+        </h1>
+        <div style={{ display: "grid", gridTemplateColumns: "20% 60%", gridGap: 5 }}>
+            <div style={{height: "55px", color: "drakgrey", marginLeft:"auto", marginRight: "auto", padding: "15px",
+    backgroundColor: "lightgrey", display: "flex", justifyContent: "center",  borderRadius: "25px", alignItems: "right", width:"50%"}} >
+                <h5 className = "subtitle is-6">
+                    3D Model
+                </h5>
+            </div>
+            <div class = "center" style={{display: "block", width: "50%", marginLeft:"auto", marginRight: "auto"}} >  
+                <img src={table} alt="Table" style={{ width: 425, height: 500}} />
+            </div>
+        </div>
+
+        </div>
+    );
+};
 
 export default Spaces;
 
